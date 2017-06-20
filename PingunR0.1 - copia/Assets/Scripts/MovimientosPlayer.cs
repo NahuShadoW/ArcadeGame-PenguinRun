000f6d0d0f18ad7b70e4f210c462c95bc5bb9d0c
Nahuel Castillo <nahushadow@gmail.com>
2017-06-20 14:59:03 -0300

    JUEGO ALPHA

--- PingunR0.1 - copia/Assets/Scripts/MovimientosPlayer.cs
+++ PingunR0.1 - copia/Assets/Scripts/MovimientosPlayer.cs
@@ -1,28 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MovimientosPlayer : MonoBehaviour {
+
+	//variable que permite darle el contador d vida al player
+	public int vida;
 
 	//variable para saltar
 	public float alturaSalto;
-	//variable para la velocidad del movimiento
-	public float velocidadMovimiento;
+
 	//permite saber que esta en el suelo
-	private bool enSuelo = true;
+	private bool enSuelo;
 
 	public Transform  comprobadorSuelo;
-	public float comprobadorRadio = 0.07f;
+
+	//permite comprobar el radio
+	public float comprobadorRadio = 4f;
+
 	//permite en que capa colosionara el comprobadorSuelo
 	public LayerMask mascaraSuelo;
 
-	
+	public AudioSource sonidoSalto;
+	public AudioSource sonidoMuerte;
+
+	void Start () {
+
+
+	}
 
 	void FixedUpdate(){
 
 		//Sirve para poder hacer que el persona tenga permitido el salto solo cuando esta tocando el suelo
 		enSuelo = Physics2D.OverlapCircle (comprobadorSuelo.position, comprobadorRadio, mascaraSuelo);
-	
+
 	}
 
 	void Update () {
@@ -32,16 +43,30 @@ public class MovimientosPlayer : MonoBehaviour {
 		if(enSuelo && Input.GetKeyDown(KeyCode.Space))
 		{
 			GetComponent<Rigidbody2D>().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x, alturaSalto);
+
 		}
-		//Mover a la derecha , usamos tecla D
-		if(Input.GetKey (KeyCode.D)) 
-		{
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (velocidadMovimiento, GetComponent<Rigidbody2D>().velocity.y);
-		}	
-		//Mover a la izquierda , usamos tecla A
-		if(Input.GetKey (KeyCode.A)) 
-		{
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-velocidadMovimiento, GetComponent<Rigidbody2D>().velocity.y);
-		}	
+
 	}
+		
+	public void RecibirDaño (int dañoRecibido){
+
+		if (vida != 0) {
+			vida -= dañoRecibido;
+			print ("Has recibido daño de Walrus!, tu vida es: " + vida);
+		} 
+		else if(vida <= 0) {
+			Muerte ();
+		}
+
+	}
+
+	//Fin del juego
+	public void Muerte(){
+
+		SceneManager.LoadScene("GameOverScene");
+		Debug.Log("Has muerto");
+		//sonidoMuerte.Play();
+	}
+
+
 }
---
+++ PingunR0.1 - copia/Assets/Scripts/Puntaje.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Puntaje : MonoBehaviour {
+
+    public float puntajeGanado;
+    public Text textoDePuntaje;
+	public Text gameOverPuntaje;
+
+	void Awake (){
+
+
+		DontDestroyOnLoad (transform.gameObject);
+	}
+
+	void Start () {
+        puntajeGanado = 0f;
+		
+	}
+	
+	void Update () {
+
+
+        puntajeGanado = puntajeGanado + Time.deltaTime * 100;
+    	Escribir();
+		
+	}
+
+    void Escribir() {
+		
+        puntajeGanado = (int)puntajeGanado;
+        textoDePuntaje.text = "Score: " + puntajeGanado;
+
+
+    }
+
+
+
+}
